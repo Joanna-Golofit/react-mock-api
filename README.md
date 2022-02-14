7fda3db3e79525a93a84fe7684a667796227fd54
Joanna-Golofit <sunderlajsa@gmail.com>
2022-02-14 15:39:16 +0100

    serwerek i pierwszy fetch i wyswietlenie danych

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# react-mock-api
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app), using [clean-cra Template](https://github.com/JorgePasco1/cra-template-clean-cra).
+
+https://www.youtube.com/watch?v=Xw3K6Kk5Npw&ab_channel=Classsed
+
+## Available Scripts
+
+npx create-react-app . --template clean-cra
+npm i miragejs -D
+npm install bootstrap
+
---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+function App() {
+  const [movies, setMovies] = useState(null);
+  useEffect(() => {
+    fetch("/api/movies")
+      .then((res) => res.json())
+      .then((json) => setMovies(json.movies))
+      // .then(console.log(movies))
+      .then(console.log("muvisy:", movies))
+      .catch((err) => console.log(err.message));
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="row justify-contetn-center">
+        <div className="col">
+          {movies?.length > 0 ? (
+            movies.map((movie) => (
+              <p key={movie.id}>
+                {movie.name} in {movie.year}
+              </p>
+            ))
+          ) : (
+            <p>nie ma filmow</p>
+          )}
+        </div>
+      </div>
+      Hello World
+    </div>
+  );
+}
+
+export default App;
